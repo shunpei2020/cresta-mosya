5258119a416b1c969f02355ee3efa733cfb505c3
shunpei Tanaka <73131022+shunpei2020@users.noreply.github.com>
2021-03-25 15:20:08 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,44 @@
-$('.burger').on('click', function(){
-  $('.nav-links').toggleClass('nav-active');
-  $(this).toggleClass('toggle');
-  $('body').toggleClass('noscroll')
-});
-$('.nav-links li').on('click', function(){
+$(function() {
+  let imgHeight = $('.kv-wrapper').outerHeight();
+  let bgHeight = $('.fv').outerHeight();
+
+  $('.burger').on('click', function(){
+    if ($(window).scrollTop() < imgHeight -50) {
+      $('.nav-links').toggleClass('nav-active');
+      $(this).toggleClass('toggle');
+      $('.burger-mask').fadeToggle('300');
+      $('body').toggleClass('noscroll');
+    } else {
+      $(this).toggleClass('black');
+      $('.menu').toggleClass('black');
+      $('.nav-links').toggleClass('nav-active');
+      $(this).toggleClass('toggle');
+      $('.burger-mask').fadeToggle('300');
+      $('body').toggleClass('noscroll');
+    }
+  });
+
+  $('.nav-links li').on('click', function(){
   $('.nav-links').removeClass('nav-active');
   $('.burger').removeClass('toggle')
   $('body').removeClass('noscroll');
+  });
+
+  $(window).on('load scroll', function(){
+    if ($(window).scrollTop() < imgHeight -50) {
+      $('.logo').removeClass('black');
+    } else {
+      $('.logo').addClass('black');
+    }
+
+    if ($(window).scrollTop() < bgHeight -50) {
+      $('.burger').removeClass('black');
+      $('.menu').removeClass('black');
+    } else {
+      $('.burger').addClass('black');
+      $('.menu').addClass('black');
+    }
+  });
 });
 
 const smooth = () => {
